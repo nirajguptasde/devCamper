93ab7631bb2420b5d5fa82aef2d4bf3b89c7dfa4
Niraj Gupta <nirajguptasde@gmail.com>
2022-12-27 04:21:35 +0530

    Mongoose setup done with database connection to atlas

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// load env vars
+dotenv.config({path: './config/config.env'});
+
+// connect to database;
+
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-
-// load env vars
-
-dotenv.config({path: './config/config.env'});
 
 const app = express();
 
@@ -22,4 +26,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close the server & exit process
+  server.close(()=> process.exit());
+})
