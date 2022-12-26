78f82bcbadc6142d39b09a89dca57f9237ca7a4c
Niraj Gupta <nirajguptasde@gmail.com>
2022-12-27 04:28:51 +0530

    added colors to console messaages

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
 }
 
 module.exports = connectDB;
